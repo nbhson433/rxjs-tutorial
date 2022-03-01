3b6b04043b16ea514eb0c305e602f16605f04b8b
lkthien <lkthien.agim8@gmail.com>
2022-03-01 14:58:56 +0700

    update

--- 5_Lazy-Observable-UnSubscribe/app.js
+++ 5_Lazy-Observable-UnSubscribe/app.js
@@ -1,5 +1,6 @@
 const interval$ = Observable.interval(1000)
 
+// Ban than subscribe se tra ve cho chung ta mot object la subscription
 const subscription = interval$.subscribe((data) => {
   console.log(data);
 }) 
