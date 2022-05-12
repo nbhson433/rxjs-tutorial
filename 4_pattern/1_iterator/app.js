0785fbcbbe550f0af06957fb494ed80bad646d1a
Thong Ly <lminhthong@tma.com.vn>
2022-05-12 17:22:24 +0700

    Iterator pattern

--- 4_pattern/1_iterator/app.js
+++ 4_pattern/1_iterator/app.js
@@ -4,43 +4,19 @@ var Iterator = function (items) {
 }
 
 Iterator.prototype = {
-  first: function () {
-    this.reset();
-    return this.next();
-  },
-  next: function () {
-    return this.items[this.index++];
-  },
-  hasNext: function () {
-    return this.index <= this.items.length;
-  },
-  reset: function () {
-    this.index = 0;
-  },
-  each: function (callback) {
-    for (var item = this.first(); this.hasNext(); item = this.next()) {
-      callback(item);
+  myEach: function (callback) {
+    for (const key in this.items) {
+      if (Object.hasOwnProperty.call(this.items, key)) {
+        callback(this.items[key], this.index)
+        this.index++
+      }
     }
   }
 }
 
-function run() {
+var items = ["one", 2, "circle", true, "Applepie"];
+var iter = new Iterator(items);
 
-  var items = ["one", 2, "circle", true, "Applepie"];
-  var iter = new Iterator(items);
-
-  // using for loop
-
-  for (var item = iter.first(); iter.hasNext(); item = iter.next()) {
-    console.log(item);
-  }
-  console.log("");
-
-  // using Iterator's each method
-
-  iter.each(function (item) {
-    console.log(item);
-  });
-}
-
-run()
+iter.myEach(function (item, index) {
+  console.log(index, item);
+});
